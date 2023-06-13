c190954eb76c2aa9e411eaac2adeb8c4e282983f
Carlos Maximiliano <maxi.krbr@gmail.com>
2023-06-13 20:33:34 -0300

    feat: axios service created

---
+++ src/services/index.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const karsAPI = axios.create({
+  baseURL: "https://kenzie-kars.herokuapp.com",
+  timeout: 4000,
+});
+
+export const localAPI = axios.create({
+  baseURL: "http://localhost:3000/",
+  timeout: 3000,
+});
