a3e18eb74af7909afded8b3a7f0eb6096b4a35f3
Juan <juan.ortiz96@gmail.com>
2023-07-04 14:21:50 -0300

    feat: modal ad and styling

--- src/components/modals/newAdModal/index.tsx
+++ src/components/modals/newAdModal/index.tsx
@@ -1,15 +1,18 @@
 import { useContext } from 'react'
 import { AdsContext } from '../../../contexts/ads.context'
 import Input from '../../inputForAd/index'
-import { StyledForm } from './styles'
+import { StyledNewAdModal } from './styles'
 import { useForm } from 'react-hook-form'
 import { sellerCarAdSchema } from '../../../schemas/ads.schema'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { iSellerAd } from '../../../interfaces/ads.interfaces'
+import { useFilter } from '../../../contexts/filter.context'
 
 export const NewAdModal = () => {
 	const { setShowNewAdState, createAd, getSellerAds } = useContext(AdsContext)
 
+	const {brandList, modelList, setModelList, getModel} = useFilter()
+	
 	const {
 		register,
 		handleSubmit,
@@ -23,192 +26,195 @@ export const NewAdModal = () => {
 	}
 
 	return (
-		<div>
-			<StyledForm
-				noValidate
-				onSubmit={handleSubmit(onSubmit)}
-			>
-				<div className='divTitleAndCloseButton'>
-					<h2>Criar anúncio</h2>
-					<button onClick={setShowNewAdState}>X</button>
-				</div>
-
-				<div className='divInputs'>
-					<h3>Informações do veículo:</h3>
-					<Input
-						id='brand'
-						type='text'
-						placeholder='Mercedez-Benz'
-						label='Marca'
-						register={register('brand')}
-					/>
-					{errors.brand && (
-						<span className='alert-span'>
-							{errors.brand.message}
-						</span>
-					)}
-					<Input
-						id='model'
-						type='text'
-						placeholder='A 200 CGI ADVANCE SEDAN'
-						label='Modelo'
-						register={register('model')}
-					/>
-					{errors.model && (
-						<span className='alert-span'>
-							{errors.model.message}
-						</span>
-					)}
-					<div className='divTwoInputs'>
-						<Input
-							id='year'
-							type='number'
-							placeholder='2018'
-							label='Ano'
-							register={register('year')}
-						/>
-						{errors.year && (
-							<span className='alert-span'>
-								{errors.year.message}
-							</span>
-						)}
-						<Input
-							id='fuel'
-							type='text'
-							placeholder='Gasolina / Etanol'
-							label='Combustível'
-							register={register('fuel')}
-						/>
-						{errors.fuel && (
-							<span className='alert-span'>
-								{errors.fuel.message}
-							</span>
-						)}
+		<StyledNewAdModal>
+			<div className='modal-ad-container'>
+				<form
+					noValidate
+					onSubmit={handleSubmit(onSubmit)}
+				>
+					<div className='divTitleAndCloseButton'>
+						<h2>Criar anúncio</h2>
+						<button onClick={setShowNewAdState}>X</button>
 					</div>
-
-					<div className='divTwoInputs'>
-						<Input
-							id='mileage'
-							type='number'
-							placeholder='3000'
-							label='Quilometragem'
-							register={register('mileage')}
-						/>
-						{errors.mileage && (
-							<span className='alert-span'>
-								{errors.mileage.message}
-							</span>
-						)}
-						<Input
-							id='color'
+					<div className='divInputs'>
+						<h3>Informações do veículo:</h3>
+						{/* <Input
+							id='brand'
 							type='text'
-							placeholder='Branco'
-							label='Cor'
-							register={register('color')}
-						/>
-						{errors.color && (
+							placeholder='Mercedez-Benz'
+							label='Marca'
+							register={register('brand')}
+						/> */}
+						<label htmlFor="">Marca</label>
+						<select name="" id="">
+							{brandList.map((brand, index) => {
+								return <option value={brand} key={index} onClick={() => getModel(brand)}>{brand}</option>
+							})}
+						</select>
+						{errors.brand && (
 							<span className='alert-span'>
-								{errors.color.message}
+								{errors.brand.message}
 							</span>
 						)}
-					</div>
-
-					<div className='divTwoInputs'>
-						<Input
-							id='fipe_list_price'
+						{/* <Input
+							id='model'
 							type='text'
-							placeholder='48000'
-							label='Preço tabela FIPE'
-							register={register('fipe_list_price')}
-						/>
-						{errors.fipe_list_price && (
+							placeholder='A 200 CGI ADVANCE SEDAN'
+							label='Modelo'
+							register={register('model')}
+						/> */}
+						<label htmlFor="">Model</label>
+						{modelList.map((model)=> {
+							return <option value={model.name} key={model.id}>{model.name}</option>
+						})}
+						{errors.model && (
 							<span className='alert-span'>
-								{errors.fipe_list_price.message}
+								{errors.model.message}
 							</span>
 						)}
-
+						<div className='divTwoInputs'>
+							<Input
+								id='year'
+								type='number'
+								placeholder='2018'
+								label='Ano'
+								register={register('year')}
+							/>
+							{errors.year && (
+								<span className='alert-span'>
+									{errors.year.message}
+								</span>
+							)}
+							<Input
+								id='fuel'
+								type='text'
+								placeholder='Gasolina / Etanol'
+								label='Combustível'
+								register={register('fuel')}
+							/>
+							{errors.fuel && (
+								<span className='alert-span'>
+									{errors.fuel.message}
+								</span>
+							)}
+						</div>
+						<div className='divTwoInputs'>
+							<Input
+								id='mileage'
+								type='number'
+								placeholder='3000'
+								label='Quilometragem'
+								register={register('mileage')}
+							/>
+							{errors.mileage && (
+								<span className='alert-span'>
+									{errors.mileage.message}
+								</span>
+							)}
+							<Input
+								id='color'
+								type='text'
+								placeholder='Branco'
+								label='Cor'
+								register={register('color')}
+							/>
+							{errors.color && (
+								<span className='alert-span'>
+									{errors.color.message}
+								</span>
+							)}
+						</div>
+						<div className='divTwoInputs'>
+							<Input
+								id='fipe_list_price'
+								type='text'
+								placeholder='48000'
+								label='Preço tabela FIPE'
+								register={register('fipe_list_price')}
+							/>
+							{errors.fipe_list_price && (
+								<span className='alert-span'>
+									{errors.fipe_list_price.message}
+								</span>
+							)}
+							<Input
+								id='price'
+								type='text'
+								placeholder='50000'
+								label='Preço'
+								register={register('price')}
+							/>
+							{errors.price && (
+								<span className='alert-span'>
+									{errors.price.message}
+								</span>
+							)}
+						</div>
 						<Input
-							id='price'
+							id='description'
 							type='text'
-							placeholder='50000'
-							label='Preço'
-							register={register('price')}
+							placeholder="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+							label='Descrição'
+							register={register('description')}
 						/>
-						{errors.price && (
+						{errors.description && (
 							<span className='alert-span'>
-								{errors.price.message}
+								{errors.description.message}
 							</span>
 						)}
+						<Input
+							id='cover_image'
+							type='text'
+							placeholder='https://image.com'
+							label='Imagem da capa'
+							register={register('cover_image')}
+						/>
+						{errors.cover_image && (
+							<span className='alert-span'>
+								{errors.cover_image.message}
+							</span>
+						)}
+						<Input
+							id='gallery_image_1'
+							type='text'
+							placeholder='https://image.com/01'
+							label='Imagem da galeira'
+							register={register('gallery_image_1')}
+						/>
+						{errors.gallery_image_1 && (
+							<span className='alert-span'>
+								{errors.gallery_image_1.message}
+							</span>
+						)}
+						<Input
+							id='gallery_image_2'
+							type='text'
+							placeholder='https://image.com/02'
+							label='Imagem da galeira'
+							register={register('gallery_image_2')}
+						/>
+						{errors.gallery_image_2 && (
+							<span className='alert-span'>
+								{errors.gallery_image_2.message}
+							</span>
+						)}
+						<div className='divButtonCancelAndSubmit'>
+							<button
+								className='buttonCancel'
+								onClick={setShowNewAdState}
+							>
+								Cancelar
+							</button>
+							<button
+								type='submit'
+								className='buttonSubmit'
+							>
+								Criar Anúncio
+							</button>
+						</div>
 					</div>
-
-					<Input
-						id='description'
-						type='text'
-						placeholder="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
-						label='Descrição'
-						register={register('description')}
-					/>
-					{errors.description && (
-						<span className='alert-span'>
-							{errors.description.message}
-						</span>
-					)}
-
-					<Input
-						id='cover_image'
-						type='text'
-						placeholder='https://image.com'
-						label='Imagem da capa'
-						register={register('cover_image')}
-					/>
-					{errors.cover_image && (
-						<span className='alert-span'>
-							{errors.cover_image.message}
-						</span>
-					)}
-
-					<Input 
-						id='gallery_image_1'
-						type='text'
-						placeholder='https://image.com/01'
-						label='Imagem da galeira'
-						register={register('gallery_image_1')}
-					/>
-					{errors.gallery_image_1 && (
-						<span className='alert-span'>
-							{errors.gallery_image_1.message}
-						</span>
-					)}
-
-					<Input 
-						id='gallery_image_2'
-						type='text'
-						placeholder='https://image.com/02'
-						label='Imagem da galeira'
-						register={register('gallery_image_2')}
-					/>
-					{errors.gallery_image_2 && (
-						<span className='alert-span'>
-							{errors.gallery_image_2.message}
-						</span>
-					)}
-
-					<div className='divButtonCancelAndSubmit'>
-						<button
-							className='buttonCancel'
-							onClick={setShowNewAdState}
-						>
-							Cancelar
-						</button>
-						<button
-							type='submit'
-							className='buttonSubmit'
-						>
-							Criar Anúncio
-						</button>
-					</div>
-				</div>
-			</StyledForm>
-		</div>
+				</form>
+			</div>
+		</StyledNewAdModal>
 	)
 }
--- src/interfaces/ads.interfaces.tsx
+++ src/interfaces/ads.interfaces.tsx
@@ -1,148 +1,163 @@
-import { z } from 'zod'
-import { carAdData, carAdSchema, responseCarAdData } from '../schemas/ads.schema'
+import { z } from "zod";
+import {
+  carAdData,
+  carAdSchema,
+  responseCarAdData,
+} from "../schemas/ads.schema";
 
-export type iAd = z.infer<typeof carAdSchema>
-export type iAdsArray = Array<carAdData>
-export type iOrderedPartialAdsArray = Array<responseCarAdData>
+export type iAd = z.infer<typeof carAdSchema>;
+export type iAdsArray = Array<carAdData>;
+export type iOrderedPartialAdsArray = Array<responseCarAdData>;
 export interface CardsProps {
-	arr: iAdsArray | iOrderedPartialAdsArray
+  arr: iAdsArray | iOrderedPartialAdsArray;
 }
 
 export interface iSellerAd extends iAd {
-	gallery_image_1: string;
-	gallery_image_2: string;
+  gallery_image_1: string;
+  gallery_image_2: string;
 }
 
 export interface iCar {
-	id: string;
-	name: string;
-	brand: string;
-	year: string;
-	fuel: number;
-	value: number;
-}
-  
-export type iAdStatus = {
-	is_active: boolean
+  id: string;
+  name: string;
+  brand: string;
+  year: string;
+  fuel: number;
+  value: number;
 }
 
+export type iAdStatus = {
+  is_active: boolean;
+};
 
 interface model {
-	name: string;
+  name: string;
 }
 
 export interface iData {
-	chevrolet: model[];
-	citroën: model[];
-	fiat: model[];
-	ford: model[];
-	honda: model[];
-	hyundai: model[];
-	nissan: model[];
-	peugeot: model[];
-	renault: model[];
-	toyota: model[];
-	volkswagen: model[];
+  chevrolet: model[];
+  citroën: model[];
+  fiat: model[];
+  ford: model[];
+  honda: model[];
+  hyundai: model[];
+  nissan: model[];
+  peugeot: model[];
+  renault: model[];
+  toyota: model[];
+  volkswagen: model[];
 }
 
-
 export interface iBrand {
-	id: number
-	name: string
+  id: string;
+  name: string;
 }
 
 export interface iModel {
-	id: number
-	name: string
+  id: string;
+  name: string;
 }
 
 export interface iColor {
-	id: number
-	name: string
+  id: string;
+  name: string;
 }
 
 export interface iYear {
-	id: number
-	year: number
+  id: string;
+  year: number;
 }
 
 export interface iFuel {
-	id: number
-	name: string
+  id: string;
+  name: string;
 }
 
 export interface iAdsProps {
-	children: React.ReactNode
+  children: React.ReactNode;
+}
+
+export interface iModel {
+  id: string;
+  name: string;
+  brand: string;
+  year: string;
+  fuel: number;
+  value: number;
 }
 
 export interface iAdValues {
-	showNewAdForm: boolean
-	allAdsArray: iAd[]
-	getAllAdsArray: () => void
-	adsById: unknown
-	getAdsById: unknown
-	sellerAds: iAd[]
-	getSellerAds: () => void
-	createAd: any
-	updateAd: any
-	deleteAd: any
-	updateAdStatus: any
-	setShowNewAdState: () => void
-	filterBrand: string[]
-	setFilterBrand: React.Dispatch<React.SetStateAction<string[]>>
-	filterModel: string[]
-	setFilterModel: React.Dispatch<React.SetStateAction<string[]>>
-	filterColor: string[]
-	setFilterColor: React.Dispatch<React.SetStateAction<string[]>>
-	filterYear: string[]
-	setFilterYear: React.Dispatch<React.SetStateAction<string[]>>
-	filterFuel: string[]
-	setFilterFuel: React.Dispatch<React.SetStateAction<string[]>>
-	filterKm: string
-	setFilterKm: React.Dispatch<React.SetStateAction<string>>
-	filterPrice: string
-	setFilterPrice: React.Dispatch<React.SetStateAction<string>>
-	showModalEditAd: boolean
-	setShowModalEditAd: React.Dispatch<React.SetStateAction<boolean>>
-	adData: any
-	setAdData: React.Dispatch<React.SetStateAction<any>>
+  showNewAdForm: boolean;
+  allAdsArray: iAd[];
+  getAllAdsArray: () => void;
+  adsById: unknown;
+  getAdsById: unknown;
+  sellerAds: iAd[];
+  getSellerAds: () => void;
+  createAd: any;
+  updateAd: any;
+  deleteAd: any;
+  updateAdStatus: any;
+  setShowNewAdState: () => void;
+  filterBrand: string[];
+  setFilterBrand: React.Dispatch<React.SetStateAction<string[]>>;
+  filterModel: string[];
+  setFilterModel: React.Dispatch<React.SetStateAction<string[]>>;
+  filterColor: string[];
+  setFilterColor: React.Dispatch<React.SetStateAction<string[]>>;
+  filterYear: string[];
+  setFilterYear: React.Dispatch<React.SetStateAction<string[]>>;
+  filterFuel: string[];
+  setFilterFuel: React.Dispatch<React.SetStateAction<string[]>>;
+  filterKm: string;
+  setFilterKm: React.Dispatch<React.SetStateAction<string>>;
+  filterPrice: string;
+  setFilterPrice: React.Dispatch<React.SetStateAction<string>>;
+  showModalEditAd: boolean;
+  setShowModalEditAd: React.Dispatch<React.SetStateAction<boolean>>;
+  adData: any;
+  setAdData: React.Dispatch<React.SetStateAction<any>>;
 }
 
-
 export interface iFilterFunctions {
-	brandSelected: iBrand | null
-	showAllBrands: boolean
-	selectBrand: (brand: iBrand) => void
+  brandSelected: iBrand | null;
+  showAllBrands: boolean;
+  selectBrand: (brand: iBrand) => void;
 
-	modelSelected: iModel | null
-	showAllModels: boolean
-	selectModel: (model: iModel) => void
+  modelSelected: iModel | null;
+  showAllModels: boolean;
+  selectModel: (model: iModel) => void;
 
-	colorSelected: iColor | null
-	showAllColors: boolean
-	selectColor: (color: iColor) => void
+  colorSelected: iColor | null;
+  showAllColors: boolean;
+  selectColor: (color: iColor) => void;
 
-	yearSelected: iYear | null
-	showAllYears: boolean
-	selectYear: (year: iYear) => void
+  yearSelected: iYear | null;
+  showAllYears: boolean;
+  selectYear: (year: iYear) => void;
 
-	fuelSelected: iFuel | null
-	showAllFuels: boolean
-	selectFuel: (fuel: iFuel) => void
+  fuelSelected: iFuel | null;
+  showAllFuels: boolean;
+  selectFuel: (fuel: iFuel) => void;
 
-	minKm: number | null
-	SetMinKm: (km: number) => void
+  minKm: number | null;
+  SetMinKm: (km: number) => void;
 
-	maxKm: number | null
-	SetMaxKm: (km: number) => void
+  maxKm: number | null;
+  SetMaxKm: (km: number) => void;
 
-	minPrice: number | null
-	SetMinPrice: (price: number) => void
+  minPrice: number | null;
+  SetMinPrice: (price: number) => void;
 
-	maxPrice: number | null
-	SetMaxPrice: (price: number) => void
+  maxPrice: number | null;
+  SetMaxPrice: (price: number) => void;
+
+  brandList: string[];
+  getModel: (brand: string) => void;
+  modelList: iModel[]
+  setModelList: React.Dispatch<React.SetStateAction<iModel[]>>;
 }
 
 export interface iFilterProps {
-	children: React.ReactNode
+  children: React.ReactNode;
 }
