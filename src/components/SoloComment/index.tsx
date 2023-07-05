acca1f46fc13a89f1232af4555de430b7583bb8d
Carlos Maximiliano <maxi.krbr@gmail.com>
2023-07-05 01:58:59 -0300

    fix: style changes on comments

--- src/components/SoloComment/index.tsx
+++ src/components/SoloComment/index.tsx
@@ -5,24 +5,27 @@ export const SoloComment = ({ comment }: any) => {
     const commentTime = new Date(comment.created_at)
     const timeSince = currentTime - commentTime
     const commentCreatedSince = timeSince/(1000 * 3600 * 24)
-    if (commentCreatedSince < 1){
-        message = `há ${(commentCreatedSince * 24).toFixed(0)} horas`
+    console.log(commentCreatedSince < 1)
+    if (commentCreatedSince <= 1){
+        message = `há ${(commentCreatedSince * 24).toFixed(0)} hora(s)`
     }
-    if (commentCreatedSince < 30){
-        message = `há ${commentCreatedSince.toFixed(0)} dias`
+    if (commentCreatedSince > 1){
+        message = `há ${commentCreatedSince.toFixed(0)} dia(s)`
     }
     if (commentCreatedSince >= 30){
-        message = `há ${(commentCreatedSince / 30).toFixed(0)} meses`
+        message = `há ${(commentCreatedSince / 30).toFixed(0)} mes(es)`
     }
 
     return (
         <li>
             <div className="user-info">
-                <img src="" alt="" />
-                <h3>{comment.user_id}</h3>
-                <span></span>
-                <p>{message}</p>
-                <p>{comment.comment}</p>
+                <div className="row-div">
+                    <img src="" alt="" />
+                    <h3>{comment.user.name}</h3>
+                    <span className="comment-span"> ● </span>
+                    <p className="date-info">{message}</p>
+                </div>
+                <p className="comment-text">{comment.comment}</p>
             </div>
         </li>
     )
