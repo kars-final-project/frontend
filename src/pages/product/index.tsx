8ad4773c522e0d7e325f3e0149eba08b1c59a94c
iarley1 <iarley96@gmail.com>
2023-07-06 16:46:35 -0300

    fix whatsapp redirect

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -2,22 +2,22 @@ import Comments from "../../components/CommentsList/index";
 import Footer from "../../components/footer/index";
 import Header from "../../components/header/index";
 import { StyledProduct } from "./style";
-import UserImg from './userImg.png'
+import UserImg from "./userImg.png";
 import { AdsContext } from "../../contexts/ads.context";
 import { useContext, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { TokenData } from "interfaces/user.interface";
 import jwtDecode from "jwt-decode";
 import { useAuth } from "../../contexts/auth.context";
+import { localAPI } from "../../services";
 
 function Product() {
-  
-  const {adsById, getAdsById } = useContext(AdsContext)
-  const {id} = useParams()
-  const {getUserData, user, nameInitial} = useAuth()
-const navigate = useNavigate()
+  const { adsById, getAdsById } = useContext(AdsContext);
+  const { id } = useParams();
+  const { getUserData, user, nameInitial, userAd, setUserAd } = useAuth();
+  const navigate = useNavigate();
 
-  useEffect(()=> {
+  useEffect(() => {
     const token = localStorage.getItem("@kars_login");
     if (token) {
       const decodedToken: TokenData = jwtDecode(token);
@@ -25,12 +25,20 @@ const navigate = useNavigate()
         getUserData(+decodedToken.sub);
       }
     }
-    getAdsById(id!)
-  
-  
-      }, [])
-  
-      console.log(adsById)
+    getAdsById(id!);
+  }, []);
+
+  useEffect(() => {
+    async function getUserByIdAd(id: number | string) {
+      try {
+        const response = await localAPI.get(`users/${id}`);
+        setUserAd(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getUserByIdAd(adsById.user_id);
+  }, [adsById]);
   return (
     <StyledProduct>
       <Header />
@@ -78,7 +86,9 @@ const navigate = useNavigate()
               <div>{nameInitial}</div>
               <h3>{user?.name}</h3>
               <p>{user?.description}</p>
-              <button onClick={()=> navigate('/dashboard')}>Ver todos anuncios</button>
+              <button onClick={() => navigate("/dashboard")}>
+                Ver todos anuncios
+              </button>
             </div>
           </div>
         </div>
